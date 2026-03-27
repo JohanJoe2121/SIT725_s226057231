f202ed0266276973b4ca8aa9a7ed2597e7ee81dd
JohanJoe2121 <johanjoe2003@gmail.com>
2026-03-27 20:53:38 +1100

    Completed Task 3.2P and learnt to use Materialize components using books as an example

---
+++ 3.2P/public/js/scripts.js
@@ -0,0 +1,69 @@
+
+const cardList = [
+  {
+    title: "1984", // ! Replace with your second book title
+    image: "images/book4.png", // ! Replace with your second book image
+    desciption: "A dystopian novel that portrays a society under constant surveillance, where freedom is restricted and truth is controlled by a powerful government. It highlights the dangers of totalitarianism and loss of individuality." // ! Replace with your second book description
+  },
+  {
+    title: "The Alchemist", // ! Replace with your third book title
+    image: "images/book5.png", // ! Replace with your third book image
+    desciption: "A philosophical novel about a young shepherd named Santiago who travels in search of treasure and discovers the importance of following one’s dreams and purpose in life." // ! Replace with your third book description
+  }
+];
+
+const clickMe = () => {
+  alert("Thanks for exploring these books. Hope you find something interesting!");
+  // ! Change the alert message if needed
+};
+
+const submitForm = () => {
+  let formData = {};
+  formData.first_name = $('#first_name').val();
+  formData.last_name = $('#last_name').val();
+  formData.password = $('#password').val();
+  formData.email = $('#email').val();
+
+  console.log("Form Data Submitted: ", formData);
+};
+
+const addCards = (items) => {
+  items.forEach(item => {
+    let itemToAppend =
+      '<div class="col s4 center-align">' +
+        '<div class="card medium">' +
+          '<div class="card-image waves-effect waves-block waves-light">' +
+            '<img class="activator" src="' + item.image + '">' +
+          '</div>' +
+          '<div class="card-content">' +
+            '<span class="card-title activator grey-text text-darken-4">' +
+              item.title +
+              '<i class="material-icons right">more_vert</i>' +
+            '</span>' +
+          '</div>' +
+          '<div class="card-reveal">' +
+            '<span class="card-title grey-text text-darken-4">' +
+              item.title +
+              '<i class="material-icons right">close</i>' +
+            '</span>' +
+            '<p class="card-text">' + item.desciption + '</p>' +
+          '</div>' +
+        '</div>' +
+      '</div>';
+
+    $("#card-section").append(itemToAppend);
+  });
+};
+
+$(document).ready(function () {
+  $('.materialboxed').materialbox();
+
+  $('#formSubmit').click(() => {
+    submitForm();
+  });
+
+  addCards(cardList);
+
+  $('.modal').modal();
+});
+
