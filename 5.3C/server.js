2e1e2aa5f73d064768b01a25a770726771a5e097
JohanJoe2121 <johanjoe2003@gmail.com>
2026-04-10 21:40:34 +1000

    Completed Task 5.3C and added MongoDB to existing website done in MVC architecture.

---
+++ 5.3C/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = 3000;
+
+mongoose.connect('mongodb://127.0.0.1:27017/sit725db');
+
+mongoose.connection.on('connected', () => {
+  console.log('Connected to MongoDB');
+});
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const booksRoutes = require('./routes/books.routes');
+app.use('/api/books', booksRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
